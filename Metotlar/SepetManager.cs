33ac087819c531813bcb8aa21976853494d07ea2
DALMAZ <DALMAZ@DALMAZ>
2021-01-20 15:00:23 +0300

    16 Ocak 3. ders sonu

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    //Bir class ın sonunda olan (Manager, Service, Dal, Controller) gibi ifadeler operasyon tutarlar. 
+    class SepetManager
+    {
+        //naming convention
+        //c# da () görürsek orda metot çalışır demektir
+        //Python daki def = pubic void
+        //Fonksiyon ve metot aynı şeye çıkar
+        //() içindeki Urun urun parametredir sepete neyi ekleyeceğimizi seçmiş olduk
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Sepete Eklendi : " + urun.Adi);
+        }
+
+        public void Ekle2(String urunAdi, string aciklama, int fiyat, int stokAdedi)
+        {
+            Console.WriteLine("Sepete Eklendi : " + urunAdi);
+        }
+    }
+}
---
+++ Metotlar/Urun.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class Urun
+    {
+        //Id bir datayı diğerlerşnden ayırt etmek için kullanılan eşsiz değeri anlatır(Yani tc no,soyad gibi).
+        //Property - özellik
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public double Fiyati { get; set; }
+        public string Aciklama { get; set; }
+        public int StokAdedi { get; set; }
+
+        
+
+    }
+}
+
