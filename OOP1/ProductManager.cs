24bd874f54ecb6ee15abc1c048d55fff6b4816ee
DALMAZ <DALMAZ@DALMAZ>
2021-01-20 23:54:06 +0300

    20 Ocak ders sonu

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager //İçine ürünle ilgili operasyonlar olur(ekleme, silme, listeleme vs)
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + "eklendi.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + "güncellendi.");
+        }
+
+        //void sadece yap demek sonucu başka bi yerde kullanmak istiyorsam return kullanmalıyım yoksa sonucu bi daha kullanamam
+    }
+}
